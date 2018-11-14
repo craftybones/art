99f68fe6a22ad67da55235fcd049828d340d1e46
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2018-11-14 15:14:19 +0530

    Adding a Gallery component. Index now uses the Gallery component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,35 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
+import Gallery from '../components/gallery';
 
-export default ({ data }) => {
-  let images = data.images.edges.map(e => (
-    <div>
-      <p>{e.node.author.name}</p>
-      <Img fluid={e.node.image.childImageSharp.fluid} title={e.node.title} />
-    </div>
-  ));
-  return (
-    <div style={{ width: '800px' }}>
-      <ul>
-        {images}
-        {images}
-        {images}
-      </ul>
-    </div>
-  );
-};
+export default ({ data }) => <Gallery images={data.images}/>;
 
 export const pageQuery = graphql`
-  query {
-    images: allMergedImagesJson {
-      edges {
-        node {
-          title
-          author {
-            name
+query {
+  images: allMergedImagesJson {
+    edges {
+      node {
+        title
+        author {
+          name
+        }
+        avatar {
+          childImageSharp {
+            original {
+              src
+            }
           }
-          image {
-            childImageSharp {
-              fluid(maxWidth: 800, traceSVG: { blackOnWhite: false }) {
-                ...GatsbyImageSharpFluid_tracedSVG
-              }
-              original {
-                src
-              }
+        }
+        image {
+          childImageSharp {
+            fluid(maxWidth: 300, traceSVG: { blackOnWhite: false }) {
+              ...GatsbyImageSharpFluid_tracedSVG
             }
           }
         }
       }
     }
   }
+}
 `;
