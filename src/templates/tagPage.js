f36e287b42c64671fe12fe14a3841af8e74fd85f
Srijayanth Sridhar <131062+craftybones@users.noreply.github.com>
2020-04-06 01:09:24 +0530

    Fixed tagPage to use the new Card component

--- src/templates/tagPage.js
+++ src/templates/tagPage.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 import Gallery from '../components/gallery';
+import Card from '../components/card';
 import Layout from '../layouts/index';
 
 export default ({ data }) => {
+  const cards = data.images.edges.map(x => {
+    return <Card node={x.node}></Card>;
+  });
+
   return (
     <Layout>
-      <Gallery images={data.images} />
+      <Gallery cards={cards} />
     </Layout>
   );
 };
 
 export const query = graphql`
   query($tag: String!) {
-    images: allMergedImagesJson(filter: { tags: { in: [$tag] } }, sort: {fields: [date], order: DESC}) {
+    images: allMergedImagesJson(
+      filter: { tags: { in: [$tag] } }
+      sort: { fields: [date], order: DESC }
+    ) {
       edges {
         node {
           ...GalleryPosts
